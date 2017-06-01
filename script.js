b137e260431bdc4adb8e445eb6a7a6f96733ba7b
GUEZENNEC Yves <YGUEZENNEC@tibco.fr>
2017-06-01 11:55:12 +0200

    add new model "ovni" and materials

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function createScene() {
     antialias: true
   });
   // renderer.setClearColor(new THREE.Color(0x111111, 1.0));
-  renderer.setSize(WIDTH , HEIGHT);
+  renderer.setSize(WIDTH, HEIGHT);
   document.body.appendChild(renderer.domElement);
 
 }
@@ -26,76 +26,92 @@ function createScene() {
 // creating holder for cubes
 var SpaceShip = function () {
   var that = this;
-  this.mesh;
   var loader = new THREE.JSONLoader();
-  loader.load('/models/spaceShip.json', function ( geometry, materials ) {
-		var material = new THREE.MultiMaterial(materials);
-    for (var i = 0; i < materials.length; i++) {
-        materials[i].shading = THREE.FlatShading;
-    }
-		that.mesh = new THREE.Mesh( geometry, material );
-	});
-  
-  /*var cube = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-    vertexColors: THREE.FaceColors  
+  loader.load('/models/ovni.json', function (geometry) {
+    var materials = [
+      new THREE.MeshBasicMaterial({
+        color: 0xd13a00,
+        vertexColors: THREE.FaceColors
+      }),
+      new THREE.MeshBasicMaterial({
+        color: 0x1d274c,
+        vertexColors: THREE.FaceColors
+      }),
+      new THREE.MeshBasicMaterial({
+        color: 0x1c494d,
+        vertexColors: THREE.FaceColors
+      }),
+      new THREE.MeshBasicMaterial({
+        color: 0xffb200,
+        vertexColors: THREE.FaceColors
+      }),
+      new THREE.MeshBasicMaterial({
+        color: 0xffb200,
+        vertexColors: THREE.FaceColors
+      })
+    ];
+    that.mesh = new THREE.Mesh(geometry, materials);
+    scene.add(that.mesh);
   });
-  this.mesh = new THREE.Mesh(cube, material);*/
-}
-function createSpaceShip(){ 
-	spaceShip = new SpaceShip();
-  console.log(spaceShip);
-  
-	//spaceShip.mesh.scale.set(.25,.25,.25);
-	//spaceShip.mesh.position.y = -100;
-	scene.add(spaceShip.mesh);
+
 }
 
+function createSpaceShip() {
+  spaceShip = new SpaceShip();
 
+}
 
-var mousePos={x:0, y:0};
+var mousePos = {
+  x: 0,
+  y: 0
+};
 
 // now handle the mousemove event
 
 function handleMouseMove(event) {
 
-	// here we are converting the mouse position value received 
-	// to a normalized value varying between -1 and 1;
-	// this is the formula for the horizontal axis:
-	
-	var tx = -1 + (event.clientX / WIDTH)*2;
+  // here we are converting the mouse position value received 
+  // to a normalized value varying between -1 and 1;
+  // this is the formula for the horizontal axis:
 
-	// for the vertical axis, we need to inverse the formula 
-	// because the 2D y-axis goes the opposite direction of the 3D y-axis
-	
-	var ty = 1 - (event.clientY / HEIGHT)*2;
-	mousePos = {x:tx, y:ty};
+  var tx = -1 + (event.clientX / WIDTH) * 2;
+
+  // for the vertical axis, we need to inverse the formula 
+  // because the 2D y-axis goes the opposite direction of the 3D y-axis
+
+  var ty = 1 - (event.clientY / HEIGHT) * 2;
+  mousePos = {
+    x: tx,
+    y: ty
+  };
 
 }
 
-function updatePlane(){
-	// let's move the spaceShip between -100 and 100 on the horizontal axis, 
-	// and between 25 and 175 on the vertical axis,
-	// depending on the mouse position which ranges between -1 and 1 on both axes;
-	// to achieve that we use a normalize function (see below)	
-	var targetX = normalize(mousePos.x,-1,1,-4, 4);
-	var targetY = normalize(mousePos.y,-1,1,-3, 3);
-	// Move the plane at each frame by adding a fraction of the remaining distance
-	spaceShip.mesh.position.y += (targetY - spaceShip.mesh.position.y)*0.1;
-	spaceShip.mesh.position.x += (targetX - spaceShip.mesh.position.x)*0.1;
-  // Rotate the plane proportionally to the remaining distance
-	spaceShip.mesh.rotation.z = (targetY-spaceShip.mesh.position.y)*0.1;
-	spaceShip.mesh.rotation.x = (spaceShip.mesh.position.y-targetY)*0.05;
+function updatePlane() {
+  // ajout d'un timeOut sinon ça ne marche pas le modèle est pas chargé !
+  setTimeout(function () {
+    // let's move the spaceShip between -100 and 100 on the horizontal axis, 
+    // and between 25 and 175 on the vertical axis,
+    // depending on the mouse position which ranges between -1 and 1 on both axes;
+    // to achieve that we use a normalize function (see below)	
+    var targetX = normalize(mousePos.x, -1, 1, -4, 4);
+    var targetY = normalize(mousePos.y, -1, 1, -3, 3);
+    // Move the plane at each frame by adding a fraction of the remaining distance
+    spaceShip.mesh.position.y += (targetY - spaceShip.mesh.position.y) * 0.1;
+    spaceShip.mesh.position.x += (targetX - spaceShip.mesh.position.x) * 0.1;
+    // Rotate the plane proportionally to the remaining distance
+    spaceShip.mesh.rotation.z = (targetY - spaceShip.mesh.position.y) * 0.1;
+    spaceShip.mesh.rotation.x = (spaceShip.mesh.position.y - targetY) * 0.05;
+  }, 100);
 }
 
-function normalize(v,vmin,vmax,tmin, tmax){
-	var nv = Math.max(Math.min(v,vmax), vmin);
-	var dv = vmax-vmin;
-	var pc = (nv-vmin)/dv;
-	var dt = tmax-tmin;
-	var tv = tmin + (pc*dt);
-	return tv;
+function normalize(v, vmin, vmax, tmin, tmax) {
+  var nv = Math.max(Math.min(v, vmax), vmin);
+  var dv = vmax - vmin;
+  var pc = (nv - vmin) / dv;
+  var dt = tmax - tmin;
+  var tv = tmin + (pc * dt);
+  return tv;
 }
 
 function render() {
