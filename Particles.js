a87ab7f087cbd6c11aad12a6715d55fbac900d35
GUEZENNEC Yves <YGUEZENNEC@tibco.fr>
2017-06-26 16:30:12 +0200

    add score object

--- Particles.js
+++ Particles.js
@@ -1,3 +1,4 @@
+var score = new Score();
 Particules = function () {
     var geometry = new THREE.TorusGeometry(10, 3, 16, 100);
     var sphere = new THREE.SphereGeometry(0.5, 5, 5);
@@ -56,6 +57,7 @@ Particules.prototype.collision = function (collider, x, y, z) {
         if (this.group.children[i].position.distanceTo(collider.position) <= 1.5) {
             //  this.group.children[i].material.color.setHex(0xf600ff);
             // this.group.children[i].material.color.setHex(0xf600ff);
+            score.add(1);
             this.group.children[i].visible = false;
             var explosion = new Explosion();
             scene.add(explosion.groupExplosion);
@@ -65,6 +67,7 @@ Particules.prototype.collision = function (collider, x, y, z) {
             // delete this.group.children[i];
             
         }
+        
     }
     /*this.group.children.map(function(x, i, a2){
         if(x.position.distanceTo(collider.position) <= 0.5){
---
+++ Score.js
@@ -0,0 +1,9 @@
+var Score = function() {
+    var number = document.getElementById('score');
+    
+}
+
+Score.prototype.add = function(){
+
+    number.innerHTML += 1;
+}
